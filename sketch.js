a526e3492260b3ebdaae62112eef8230c4ae9bc3
Zhibo112 <grahamliu@qq.com>
2024-09-03 14:36:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,12 @@ function setup(){
 function draw(){
     background(203, 209, 212);
     let angle = frameCount * 0.01;
-  rotateY(angle);
+  rotateZ(angle);
     orbitControl();
+    noStroke()
+    let c = color(0, 255, 42);
+  directionalLight(c, 1, -1, -1);
+    
     sphere();
 }
 
