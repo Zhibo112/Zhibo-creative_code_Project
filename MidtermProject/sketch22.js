555b84d73baae5acc894df0192df93877957b9b3
Zhibo112 <grahamliu@qq.com>
2024-10-16 11:27:14 -0500

    up

--- MidtermProject/sketch22.js
+++ MidtermProject/sketch22.js
@@ -35,7 +35,7 @@ function draw() {
     var vol = mic.getLevel();
 
     // 增加灵敏度的增益值
-    let sensitivity = 600; 
+    let sensitivity = 1100; 
     var mappedVol = map(vol, 0, 1, -sensitivity, sensitivity); 
 
     flying -= 0.1;
